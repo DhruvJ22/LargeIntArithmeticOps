8b96c8f977a3540def7ab9e1a384735a03ce8559
DhruvJ9922 <dhruvj9922@gmail.com>
2024-04-02 11:24:54 -0400

    chore: delete vscode, build/

--- build/extern/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dhruv/Desktop/LargeIntArithmeticOps/LargeIntArithmeticOps/extern/googletest/googlemock
-# Build directory: /home/dhruv/Desktop/LargeIntArithmeticOps/LargeIntArithmeticOps/build/extern/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/extern/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dhruv/Desktop/LargeIntArithmeticOps/LargeIntArithmeticOps/extern/googletest/googletest
-# Build directory: /home/dhruv/Desktop/LargeIntArithmeticOps/LargeIntArithmeticOps/build/extern/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
